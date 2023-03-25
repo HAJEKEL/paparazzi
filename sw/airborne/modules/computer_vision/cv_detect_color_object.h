83e58abae53422afe58013c8ba7c95ea96ce65e9
Thymen5225 <t.woldhuis@student.tudelft.nl>
2023-03-25 11:16:34 +0100

    Add colour values in settings of GCS

--- sw/airborne/modules/computer_vision/cv_detect_color_object.h
+++ sw/airborne/modules/computer_vision/cv_detect_color_object.h
@@ -46,8 +46,29 @@ extern uint8_t cod_cb_max2;
 extern uint8_t cod_cr_min2;
 extern uint8_t cod_cr_max2;
 
+// white
+
+extern uint8_t cod_lum_min1_white;
+extern uint8_t cod_lum_max1_white;
+extern uint8_t cod_cb_min1_white;
+extern uint8_t cod_cb_max1_white;
+extern uint8_t cod_cr_min1_white;
+extern uint8_t cod_cr_max1_white;
+
+// green
+
+extern uint8_t cod_lum_min1_green;
+extern uint8_t cod_lum_max1_green;
+extern uint8_t cod_cb_min1_green;
+extern uint8_t cod_cb_max1_green;
+extern uint8_t cod_cr_min1_green;
+extern uint8_t cod_cr_max1_green;
+
+
 extern bool cod_draw1;
 extern bool cod_draw2;
+extern bool cod_draw_white;
+extern bool cod_draw_green;
 
 // Module functions
 extern void color_object_detector_init(void);
